59dbb6dfb07dad784e4e06938d5b406d1cf54385
Emese92 <gregor.emese@gmail.com>
2022-09-27 23:49:36 +0000

    Add like and unlike function to posts

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+axios.defaults.baseURL = "https://moments-drf-api-eg.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
